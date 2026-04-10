cb5482d17cab8d345632125d0121ad67b5704b68
LejekSS <104199870+LejekSS@users.noreply.github.com>
2026-04-10 19:23:33 +0200

    Change main class for Hadoop job execution

--- run_mr.sh
+++ run_mr.sh
@@ -9,7 +9,7 @@ INPUT_DIR1=$1
 OUTPUT_DIR3=$2
 
 JAR_FILE="BigData_01-1.0-SNAPSHOT.jar"
-MAIN_CLASS="lab2.example.EsportsHeroesStatsJob"
+MAIN_CLASS="bigdata.mapreduce.main"
 
 hdfs dfs -rm -r -f "$OUTPUT_DIR3"
 hadoop jar "$JAR_FILE" "$MAIN_CLASS" "$INPUT_DIR1" "$OUTPUT_DIR3"
